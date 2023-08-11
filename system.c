8d00e5127f806cced03f931ffbfc8b72c93e9862
lilale <alessandrotambellini03@gmail.com>
2023-08-11 12:43:43 +0200

    add bash command

--- system.c
+++ system.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// NOTE: substitute <parameter> with the actual argument
+
 int main()
 {
     // delete all the executables in the project folder
@@ -16,5 +18,13 @@ int main()
 	printf("command succeded.\n");
     else
 	printf("command failed.\n");
+
+    // the output of the executable is stored in <output_file>. If <output_file> does not exist, it is created
+    res = system("<executable> <optional_argument> > <output_file>");
+    if (res == 0)
+	printf("command succeded.\n");
+    else
+	printf("command failed.\n");
+
     return 0;
 }
